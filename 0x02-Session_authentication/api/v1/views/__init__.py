818824223bdcf7c9fc37b778c6f29ee44b70e4d6
Leon12-aaryson <aaronoluk4deleonardo@gmail.com>
2024-04-19 02:37:50 +0300

    pycodestyle errors resolving and sessions

--- 0x02-Session_authentication/api/v1/views/__init__.py
+++ 0x02-Session_authentication/api/v1/views/__init__.py
@@ -2,17 +2,17 @@
 """ DocDocDocDocDocDoc
 """
 from flask import Blueprint
-
-app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
-
 from api.v1 import app
 from api.v1.views.index import *
 from api.v1.views.users import *
+from api.v1.views.session_auth import session_auth_bp
+
+
+app_views = Blueprint("app_views", __name__,
+                      url_prefix="/api/v1")
+
 
 User.load_from_file()
-
-# Import the session_auth blueprint
-from api.v1.views.session_auth import session_auth_bp
 
 # Register the session_auth blueprint
 app.register_blueprint(session_auth_bp)
