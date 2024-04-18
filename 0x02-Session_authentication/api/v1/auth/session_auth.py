78fa7a5b912aaddf05448f18efce3de60b492af0
Leon12-aaryson <aaronoluk4deleonardo@gmail.com>
2024-04-19 02:03:20 +0300

    return None for the checks on user_id

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -4,9 +4,23 @@
 from flask import request
 from typing import List, TypeVar
 from api.v1.auth.auth import Auth
+import uuid
 
 
 class SessionAuth(Auth):
     """ Session Authentication class
     """
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """ Create a Session ID for a user_id
+        """
+        if user_id is None or not isinstance(user_id, str):
+            return None
+
+        # Generate a Session ID using uuid
+        session_id = str(uuid.uuid4())
+
+        # Store the user_id mapped to the session_id
+        self.user_id_by_session_id[session_id] = user_id
+        return session_id
